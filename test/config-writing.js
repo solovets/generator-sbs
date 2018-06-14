f0ded272d8a2443e22742b375248873ded6a324e
Denis Solovets <denis.solovets@dataart.com>
2018-06-14 18:33:32 +0300

    [5] Tests for SBS config. Close #5

--- test/config-writing.js
+++ test/config-writing.js
@@ -10,23 +10,46 @@ const namingConvenions = require('../utils/namingConventions');
 const conventions = Object.keys(namingConvenions);
 
 const table = {
-    title: 'generators/config/index.js :: test of config writing'
+    title: 'generators/config/index.js :: test of creating of base structure'
 };
-
-fs.writeFileSync('test/results/config-writing.md', '', 'utf8');
-fs.appendFileSync('test/results/config-prompting.md', '###' + table.title + '\n\n_' + '_\n\n', 'utf8');
 
 describe(table.title, () => {
 
-    it('Create config files', () => {
+    it('Create bemDirectory and rootStyleFile', () => {
 
-        data.configs.forEach(config => {
-            helpers.run(path.join(__dirname, '../generators/config'))
-                .inDir('./some-test-dir')
-                .withOptions({ testmode: true })
-                .withPrompts(config);
-        });
+        return helpers.run(path.join(__dirname, '../generators/config'))
+            .inDir('./some-test-dir')
+            .withOptions({ testmode: true })
+            .withPrompts({
+                "namingConvention": "classic",
+                "useCollections": false,
+                "createBemDirectory": true,
+                "bemDirectory": path.normalize('test/results/configs/dir-should-be-created'),
+                "ext": "scss",
+                "createRootStylesFile": true,
+                "rootStylesFile": "file-should-be-created.scss",
+                "prefixForElement": "__",
+                "prefixForModifier": "_"
+            });
 
     });
 
+    it('Create rootStyleFile (bemDirectory exists)', () => {
+
+        return helpers.run(path.join(__dirname, '../generators/config'))
+            .inDir('./some-test-dir')
+            .withOptions({ testmode: true })
+            .withPrompts({
+                "namingConvention": "classic",
+                "useCollections": false,
+                "createBemDirectory": false,
+                "bemDirectory": path.normalize('test/results/configs/dir-exists'),
+                "ext": "scss",
+                "createRootStylesFile": true,
+                "rootStylesFile": "file-should-be-created.scss",
+                "prefixForElement": "__",
+                "prefixForModifier": "_"
+            });
+
+    });
 });
