63135e3c88c28849a8d683eedde25cd976262d98
Denis Solovets <denis.solovets@dataart.com>
2018-08-06 14:36:28 +0300

    [2] Code of sbs generator updated. Closes #2

---
+++ utils/components-emuns.js
@@ -0,0 +1,10 @@
+module.exports = {
+    BLOCK: 1,
+    ELEMENT: 2,
+    MODIFIER: 3,
+    props: {
+        1: {name: 'block', value: 1},
+        2: {name: 'element', value: 2},
+        3: {name: 'modifier', value: 3}
+    }
+};
